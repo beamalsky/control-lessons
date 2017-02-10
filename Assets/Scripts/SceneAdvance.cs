c416fe3d5eea26972bd13e6773864439c20c4997
beamalsky <beamalsky@gmail.com>
2017-02-10 16:10:29 -0600

    Lesson 3
    plus start of level advance button

---
+++ Assets/Scripts/SceneAdvance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneAdvance : MonoBehaviour {
+
+	private int x
+
+	void Start () {
+		
+		x = Scene.buildIndex
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
